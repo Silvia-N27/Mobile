c3f6ede8b36e2996c698e0049966373df8db1cf4
Silvia <silviadsneri@gmail.com>
2026-04-01 09:10:36 -0300

    feat: ajustes na pasta invertexto

--- invertexto/lib/view/home_page.dart
+++ invertexto/lib/view/home_page.dart
@@ -20,41 +20,52 @@ class HomePage extends StatelessWidget {
             ),
           ],
         ),
+        centerTitle: true,
       ),
       backgroundColor: Colors.black,
-      body: Padding(padding: EdgeInsets.all(10.0),
-      child: Column(
-        children: <Widget>[
-          GestureDetector(
-            child: Row(children: <Widget>[
-              Icon(Icons.edit, color: Colors.white, size:50.0),
-              SizedBox(width: 30),
-              Text("Por Extenso", style: TextStyle(color: Colors.white, fontSize: 20.0),)
-            ],
-            ),
-            onTap:() => {
-              Navigator.push(
-                context, 
-                MaterialPageRoute(builder: (context) => PorExtenso()),
+      body: Padding(
+        padding: EdgeInsets.all(10.0),
+        child: Column(
+          children: <Widget>[
+            GestureDetector(
+              child: Row(
+                children: <Widget>[
+                  Icon(Icons.edit, color: Colors.white, size: 50.0),
+                  SizedBox(width: 30),
+                  Text(
+                    "Por Extenso",
+                    style: TextStyle(color: Colors.white, fontSize: 20.0),
+                  ),
+                ],
               ),
-            },
-          ),
-          GestureDetector(
-            child: Row(children: <Widget>[
-              Icon(Icons.home, color: Colors.white, size:50.0),
-              SizedBox(width: 30),
-              Text("Busca CEP", style: TextStyle(color: Colors.white, fontSize: 20.0),)
-            ],
+              onTap: () => {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => PorExtenso()),
+                ),
+              },
             ),
-            onTap:() => {
-              Navigator.push(
-                context, 
-                MaterialPageRoute(builder: (context) => BuscaCep()),
+            GestureDetector(
+              child: Row(
+                children: <Widget>[
+                  Icon(Icons.home, color: Colors.white, size: 50.0),
+                  SizedBox(width: 30),
+                  Text(
+                    "Busca CEP",
+                    style: TextStyle(color: Colors.white, fontSize: 20.0),
+                  ),
+                ],
               ),
-            },
-          ),
-        ],
-      )),
+              onTap: () => {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => BuscaCep()),
+                ),
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
