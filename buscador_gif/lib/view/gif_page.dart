0a74dd90aa792ae72129c3fec0303c27cc8a2231
Silvia <silviadsneri@gmail.com>
2026-04-17 10:45:12 -0300

    feat:modificações

--- buscador_gif/lib/view/gif_page.dart
+++ buscador_gif/lib/view/gif_page.dart
@@ -6,6 +6,24 @@ class GifPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(Icons.arrow_back, color: Colors.white),
+        ),
+        title: Image.network(
+          "https://developers.giphy.com/branch/master/static/header-logo-0fec0225d189bc0eae27dac3e3770582.gif",
+        ),
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Image.network(gifData["images"]["fixed_height"]["url"]),
+      ),
+    );
   }
 }
